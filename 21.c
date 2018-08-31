80d088c0f89ec44eb08fa6abcdbdc8faeaf2411c
Emma Sanders <19sanders@stfrancishs.org>
2018-08-31 19:53:16 +0000

    init

---
+++ 21.c
@@ -0,0 +1,10 @@
+/* A Program that Uses the Characters, Integers, and Floating-Point Data Types */
+#include <stdio.h>
+int main (void)
+{
+printf ("I am learning the %c programming language\n", 2);
+printf ("I have just completed Chapter %d\n", 2);
+printf ("I am %.1f percent ready to move on ", 99.9);
+printf ("to the next chapter!\n");
+return 0;
+}
